757938ba2e039ee2cacbf2c4e65705cbc0e37784
Rendall Koski <rendall@gmail.com>
2021-08-12 21:19:53 +0300

    Normalize project name to Icebreakers

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# icebreakers
+# Icebreakers
+
 A list of team-building, icebreaking questions, presented randomly
+
+Try it out: <https://rendall.github.io/icebreakers/>
+
+## Add/Edit/Remove Questions
+
+To the bottom of <QUESTIONS.md>, add a _Credit_ line followed by all the questions you want to add, one per line.
 
 
 ## Development
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 type Credit = { name: string; href: string };
 type Question = { question: string; credit: Credit };
 
-const removeListMarkdown = (line: string) => line.slice(line.indexOf("*") + 1).trim();
+const removeListMarkdown = (line: string) =>
+  line.slice(line.indexOf("*") + 1).trim();
 const parseCredit = (line: string) => {
-	const match = RegExp(/Credit:\s*\[(.*)\]\((.*\))/gm).exec(line);
-	return { name: match[1], href: match[2] };
+  const match = RegExp(/Credit:\s*\[(.*)\]\((.*\))/gm).exec(line);
+  return { name: match[1], href: match[2] };
 };
-
 
 const loadQuestion = () =>
   fetch("/QUESTIONS.md").then((response) => response.text());
 
 const parseQuestions = (questionsFile: string): Question[] => {
-  const lines = questionsFile.split("\n").map(removeListMarkdown);
+  const lines = questionsFile
+    .split("\n")
+    .filter((line) => line.match(/^\s*\*/))
+    .map(removeListMarkdown);
   const parsedLines: [Credit, Question[]] = lines.reduce<[Credit, Question[]]>(
     ([credit, questions]: [Credit, Question[]], line: string) => {
       if (line.startsWith("Credit:")) {
@@ -20,47 +23,46 @@ const parseQuestions = (questionsFile: string): Question[] => {
         return [newCredit, questions];
       } else return [credit, [...questions, { question: line, credit }]];
     },
-    [{name:"", href:""}, []]
+    [{ name: "", href: "" }, []]
   );
 
   return parsedLines[1];
 };
 
-const randSort = () => Math.floor(Math.random() * 2) -1;
-const shuffleQuestions = (questions: Question[]) => questions.sort(randSort)
+const randSort = () => Math.floor(Math.random() * 2) - 1;
+const shuffleQuestions = (questions: Question[]) => questions.sort(randSort);
 
 const createUI = (
   questions: { question: string; credit: { name: string; href: string } }[]
 ) => {
+  let index = 0;
 
-	let index = 0
+  const displayQuestion = (question: Question) => {
+    const questionDisplay = document.querySelector(
+      "#question-display"
+    ) as HTMLParagraphElement;
+    questionDisplay.innerHTML = question.question;
 
-	const displayQuestion = (question: Question) => {
-		const questionDisplay = document.querySelector("#question-display") as HTMLParagraphElement;
-		questionDisplay.innerHTML = question.question;
+    const creditLink = document.querySelector(
+      "#credit-link"
+    ) as HTMLAnchorElement;
+    creditLink.href = question.credit.href;
+    creditLink.innerHTML = question.credit.name;
+  };
 
-		const creditLink = document.querySelector("#credit-link") as HTMLAnchorElement;
-		creditLink.href = question.credit.href;
-		creditLink.innerHTML = question.credit.name;
-	}
+  displayQuestion(questions[0]);
 
-	displayQuestion(questions[0]);
-
-	const reloadButton = document.querySelector("#reload-button") as HTMLButtonElement;
-	reloadButton.addEventListener("click", () => {
-		index++
-		displayQuestion( questions[index] )
-	})
+  const reloadButton = document.querySelector(
+    "#reload-button"
+  ) as HTMLButtonElement;
+  reloadButton.addEventListener("click", () => {
+    index++;
+    displayQuestion(questions[index]);
+  });
 };
 
-const onLoad = () => loadQuestion().then(parseQuestions).then(shuffleQuestions).then(createUI);
+const onLoad = () =>
+  loadQuestion().then(parseQuestions).then(shuffleQuestions).then(createUI);
 
-// parse QUESTIONS.md
-// array of { question, credit }
-// create UI
-// question display
-// reload button
-// credit field
-// present a random question
 
 onLoad();
