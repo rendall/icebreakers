61e90ef0442f4d5673eeb9d56ebbd92cb47322e6
Rendall Koski <rendall@gmail.com>
2021-08-12 16:15:28 +0300

    Move dist folder to project root

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const config = {
   entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./"),
   },
   devServer: {
     open: true,
