71d942abba17c9e055213fc1391b3a529e0f375c
Rendall <rendall@gmail.com>
2021-10-01 10:49:21 +0300

    Add theme tests

---
+++ src/themes.spec.ts
@@ -0,0 +1,41 @@
+import { readFileSync } from "fs";
+describe("THEMES.md", () => {
+  const themesFile = readFileSync("THEMES.md", "utf8");
+  const themes = themesFile.split("\n").filter((line) => line.startsWith("- "));
+
+  it("should exist", () => {
+    expect(themesFile).toBeDefined();
+  });
+
+  describe("should only contain properly formatted themes", () => {
+    themes.forEach((theme) => {
+      const themeExp = new RegExp(
+        /^- _([\w \d]*)_ ([\w#]*) ([\w#]*) ([\w#]*)$/
+      ).exec(theme);
+      const themeName = themeExp?.[1];
+      it(`'${themeName}' should have a name`, () => {
+        // Theme name should be a string between two underscores: _Name Example_
+        expect(themeName).toBeDefined();
+      });
+
+      it(`'${themeName}' should have a background color`, () => {
+        // Background color should be a properly-formatted hex color code
+        const backgroundColor = themeExp?.[2];
+        expect(backgroundColor).toBeDefined();
+        expect(backgroundColor).toMatch(/^#[\dA-F]{3,6}$/i);
+      });
+      it(`'${themeName}' should have a foreground color`, () => {
+        // Foreground color should be a properly-formatted hex color code
+        const foregroundColor = themeExp?.[3];
+        expect(foregroundColor).toBeDefined();
+        expect(foregroundColor).toMatch(/^#[\dA-F]{3,6}$/i);
+      });
+      it(`'${themeName}' should have a highlight color`, () => {
+        // highlight color should be a properly-formatted hex color code
+        const highlightColor = themeExp?.[4];
+        expect(highlightColor).toBeDefined();
+        expect(highlightColor).toMatch(/^#[\dA-F]{3,6}$/i);
+      });
+    });
+  });
+});
